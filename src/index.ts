d4054e609d8751518c8dd7d1d7e658ed2922b2aa
green1052 <34261355+green1052@users.noreply.github.com>
2022-11-23 04:11:11 +0900

    코드 개선

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,22 @@ import {filter} from "./core/filtering";
 
 import "./core/updateCheck";
 
-(() => {
-    log("🍊⚓ Initializing DCRefresher Reborn.");
+log("🍊⚓ Initializing DCRefresher Reborn.");
 
-    const loadStart = performance.now();
+const loadStart = performance.now();
 
-    const context = require.context("./modules/", true, /\.ts$/);
+const context = require.context("./modules/", true, /\.ts$/);
 
-    Promise.all(context.keys().map((v: string) => context(v).default))
-        .then((v: RefresherModule[]) => modules.load(...v))
-        .then(async () => {
-            log(
-                `🍊✔️ DCRefresher Reborn Module Loaded. took ${(
-                    performance.now() - loadStart
-                ).toFixed(2)}ms.`
-            );
+Promise.all(context.keys().map((v: string) => context(v).default))
+    .then((v: RefresherModule[]) => modules.load(...v))
+    .then(() => {
+        log(
+            `🍊✔️ DCRefresher Reborn Module Loaded. took ${(
+                performance.now() - loadStart
+            ).toFixed(2)}ms.`
+        );
 
-            filter.run();
-        });
-
-    window.addEventListener("load", async () => {
         filter.run();
     });
-})();
+
+window.addEventListener("load", filter.run);
