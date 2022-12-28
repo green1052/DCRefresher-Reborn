cfdb774c040768b5a6cbf3182aa94f702124a579
green1052 <34261355+green1052@users.noreply.github.com>
2022-12-28 22:59:43 +0900

    댓글 프레임 최적화

--- src/components/user.vue
+++ src/components/user.vue
@@ -35,7 +35,7 @@ export default Vue.extend({
     },
     computed: {
         title(): string {
-            return `${this.user.isMember() ? this.user.id : `(${this.user.ip})${this.user.ip_data ? ` [${this.user.ip_data}]` : ""}`}`;
+            return `${this.user.isMember() ? `(${this.user.id})` : `(${this.user.ip})${this.user.ip_data ? ` [${this.user.ip_data}]` : ""}`}`;
         },
 
         userInfo(): string {
