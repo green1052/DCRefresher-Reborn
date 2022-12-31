d6ebb7e0088beb5bb3e804e236e0db0549c37542
green1052 <34261355+green1052@users.noreply.github.com>
2022-12-31 22:42:05 +0900

    최초 설치, 업데이트 방식 수정 및 개선

--- src/core/updateCheck.ts
+++ src/core/updateCheck.ts
@@ -2,13 +2,27 @@ import browser from "webextension-polyfill";
 import * as Toast from "../components/toast";
 import storage from "../utils/storage";
 
-setTimeout(async () => {
-    const currentVersion = browser.runtime.getManifest().version;
-    const key = await storage.get("refresher.lastVersion");
+(async () => {
+    const installed = await storage.get<undefined | boolean>("refresher.firstInstall");
+    const updated = await storage.get<boolean>("refresher.updated");
 
-    if (currentVersion && (!key || key !== currentVersion)) {
+    if (installed === undefined && !updated) return;
+
+    setTimeout(() => {
+        const currentVersion = browser.runtime.getManifest().version;
+
+        let content: string;
+
+        if (installed === true) {
+            content = `DCRefresher Reborn ${currentVersion}이 설치되었습니다, 오류 및 개선사항은 여기에서 알려주세요.`;
+            storage.remove("refresher.firstInstall");
+        } else {
+            content = `DCRefresher Reborn이 ${currentVersion}(으)로 업데이트되었습니다, 변경 사항은 여기에서 볼 수 있습니다.`;
+            storage.set("refresher.updated", false);
+        }
+
         Toast.show(
-            `DCRefresher Reborn이 ${currentVersion}(으)로 업데이트되었습니다. 변경 사항은 여기에서 볼 수 있습니다.`,
+            content,
             false,
             4000,
             () => {
@@ -18,7 +32,5 @@ setTimeout(async () => {
                 );
             }
         );
-
-        storage.set("refresher.lastVersion", currentVersion);
-    }
-}, 5000);
+    }, 5000);
+})();
