b878742fdb280f893efb261a9adc2f9565aeb445
green1052 <34261355+green1052@users.noreply.github.com>
2023-02-20 02:39:23 +0900

    미리보기 디시콘 작성 불가 오류 수정, 댓글 로직 개선

--- src/utils/comment.ts
+++ src/utils/comment.ts
@@ -2,65 +2,68 @@ import * as http from "./http";
 import ky from "ky";
 import type { Nullable } from "./types";
 
-const _d = function (r: string) {
-    const i =
-        "yL/M=zNa0bcPQdReSfTgUhViWjXkYIZmnpo+qArOBs1Ct2D3uE4Fv5G6wHl78xJ9K";
+const rKey =
+    "yL/M=zNa0bcPQdReSfTgUhViWjXkYIZmnpo+qArOBs1Ct2D3uE4Fv5G6wHl78xJ9K";
+const rRegex = /[^A-Za-z0-9+/=]/g;
 
-    let a,
-        e,
-        n,
-        t,
-        f,
-        d,
-        h,
-        o = "",
-        c = 0;
-    for (r = r.replace(/[^A-Za-z0-9+/=]/g, ""); c < r.length; )
-        (t = i.indexOf(r.charAt(c++))),
-            (f = i.indexOf(r.charAt(c++))),
-            (d = i.indexOf(r.charAt(c++))),
-            (h = i.indexOf(r.charAt(c++))),
-            (a = (t << 2) | (f >> 4)),
-            (e = ((15 & f) << 4) | (d >> 2)),
-            (n = ((3 & d) << 6) | h),
-            (o += String.fromCharCode(a)),
-            64 != d && (o += String.fromCharCode(e)),
-            64 != h && (o += String.fromCharCode(n));
+const decode = (r: string) => {
+    let a;
+    let e;
+    let n;
+    let t;
+    let f;
+    let d;
+    let h;
+    let o = "";
+    let c = 0;
+
+    for (r = r.replace(rRegex, ""); c < r.length; ) {
+        t = rKey.indexOf(r.charAt(c++));
+        f = rKey.indexOf(r.charAt(c++));
+        d = rKey.indexOf(r.charAt(c++));
+        h = rKey.indexOf(r.charAt(c++));
+        a = (t << 2) | (f >> 4);
+        e = ((15 & f) << 4) | (d >> 2);
+        n = ((3 & d) << 6) | h;
+        o += String.fromCharCode(a);
+
+        if (d !== 64) o += String.fromCharCode(e);
+
+        if (h !== 64) o += String.fromCharCode(n);
+    }
+
     return o;
 };
 
 const requestBeforeServiceCode = (dom: Document) => {
-    let _r: string;
+    const script = dom.querySelector("#reply-setting-tmpl + script");
 
-    const _rpre = dom.querySelector(
-        `#container > section #reply-setting-tmpl + script[type="text/javascript"]`
-    );
+    if (!script) throw "_r 값을 찾을 수 없습니다.";
 
-    if (!_rpre) throw "_r 값을 찾을 수 없습니다.";
+    const dValue = script.innerHTML.match(/_d\('(.*)'\)/)?.[1];
 
-    _r = _rpre.innerHTML;
+    if (!dValue) throw "_d 값을 찾을 수 없습니다.";
 
-    const _rmatch = _r.match(/_d\('(.+)'/g);
-    if (!_rmatch?.[0]) throw "_d 값을 찾을 수 없습니다.";
-
-    _r = _d(_rmatch[0].replace(/(_d\(|')/g, ""));
+    let _r = decode(dValue);
 
     if (!_r) throw "_r이 적절한 값이 아닙니다.";
 
-    let tvl = _r,
-        fi = parseInt(tvl.substr(0, 1));
-    (fi = fi > 5 ? fi - 5 : fi + 4),
-        (tvl = tvl.replace(/^./, fi.toString())),
-        (_r = tvl);
+    let fi = parseInt(_r.slice(0, 1));
+    fi = fi > 5 ? fi - 5 : fi + 4;
+
+    _r = _r.replace(/^./, fi.toString());
 
     const r = dom.querySelector<HTMLInputElement>(
         `input[name="service_code"]`
     )!.value;
 
     const _rs = _r.split(",");
+
     let t = "";
-    for (let e = 0; e < _rs.length; e++)
-        t += String.fromCharCode((2 * (_rs[e] - e - 1)) / (13 - e - 1));
+
+    for (let e = 0; e < _rs.length; e++) {
+        t += String.fromCharCode((2 * (Number(_rs[e]) - e - 1)) / (13 - e - 1));
+    }
 
     return r.replace(/(.{10})$/, t);
 };
@@ -71,15 +74,13 @@ const secretKey = (dom: Document): URLSearchParams => {
     params.set("t_vch2_chk", "");
     params.set("g-recaptcha-response", "");
 
-    for (const element of Array.from(
-        dom.querySelectorAll<HTMLInputElement>("#focus_cmt > input")
+    for (const element of dom.querySelectorAll<HTMLInputElement>(
+        "#focus_cmt > input"
     )) {
         const id = element.getAttribute("name") ?? element.id;
 
-        if (["service_code", "gallery_no", "clickbutton"].includes(id))
-            continue;
-
-        params.set(id, element.value);
+        if (!["service_code", "gallery_no", "clickbutton"].includes(id))
+            params.set(id, element.value);
     }
 
     return params;
@@ -111,29 +112,39 @@ export async function submitComment(
 
     const params = new URLSearchParams(secretKey(dom));
     params.set("service_code", code);
-    params.set("id", preData.gallery);
     params.set("c_gall_id", preData.gallery);
-    params.set("no", preData.id);
-    if (reply) params.set("c_no", reply);
     params.set("c_gall_no", preData.id);
+
+    params.set("id", preData.gallery);
+    params.set("no", preData.id);
+
+    if (reply) params.set("c_no", reply);
+
     params.set("name", user.name);
     if (user.pw) params.set("password", user.pw);
+
     if (captcha) params.set("code", captcha);
 
     if (typeof memo === "string") {
         params.set("memo", memo);
     } else {
+        params.set("input_type", "comment");
         params.set("package_idx", memo.package_idx);
         params.set("detail_idx", memo.detail_idx);
     }
 
     const response = await ky
-        .post(http.urls.comments_submit, {
-            headers: {
-                "X-Requested-With": "XMLHttpRequest"
-            },
-            body: params
-        })
+        .post(
+            typeof memo === "string"
+                ? http.urls.comments_submit
+                : http.urls.dccon_comments_submit,
+            {
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest"
+                },
+                body: params
+            }
+        )
         .text();
 
     const [result, message] = response.split("||");
