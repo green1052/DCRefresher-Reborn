5e4ca364c21f807d9d6ab60f5e9e5cbc18eecd84
green1052 <green1052@pm.me>
2023-04-02 22:22:37 +0900

    final 제거

--- src/views/components/refresher.vue
+++ src/views/components/refresher.vue
@@ -60,9 +60,7 @@
                     <div class="text">
                         <h3>DCRefresher Reborn</h3>
                         <p>
-                            <span class="version"
-                                >{{ getVersion() }} (Final)</span
-                            >
+                            <span class="version">{{ getVersion() }}</span>
                             <a
                                 v-for="link in links"
                                 @click="open(link.url)"
