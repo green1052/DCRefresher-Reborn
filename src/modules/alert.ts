d11c2ca92876795eb7e29b9369e51024ceb65d5d
green1052 <green1052@pm.me>
2023-04-23 22:11:59 +0900

    1

--- src/modules/alert.ts
+++ src/modules/alert.ts
@@ -1,11 +1,11 @@
 import * as Toast from "../components/toast";
-import browser from "webextension-polyfill";
+import { runtime } from "webextension-polyfill";
 
 const inject = (filename: string) => {
     const scriptElement = document.createElement("script");
-    scriptElement.src = browser.runtime.getURL(filename);
+    scriptElement.src = runtime.getURL(filename);
     document.documentElement.appendChild(scriptElement);
-}
+};
 export default {
     name: "알림 개선",
     description: "디시인사이드 알림을 더 미려하게 개선합니다.",
@@ -13,20 +13,24 @@ export default {
     enable: false,
     default_enable: false,
     func() {
-        inject('../assets/js/alert_register.js');
+        inject("../assets/js/alert_register.js");
 
-        window.addEventListener("message", (event) => {
-            if (event.source !== window) {
-              return;
-            }
-          
-            if (event.data.type && (event.data.type === "ALERT")) {
-                Toast.show(event.data.text, false, 2600);
-            }
-        }, false);
+        window.addEventListener(
+            "message",
+            (event) => {
+                if (event.source !== window) {
+                    return;
+                }
+
+                if (event.data.type && event.data.type === "ALERT") {
+                    Toast.show(event.data.text, false, 2600);
+                }
+            },
+            false
+        );
     },
     revoke() {
-        inject('../assets/js/alert_unregister.js');
+        inject("../assets/js/alert_unregister.js");
     }
 } as RefresherModule<{
     require: [];
