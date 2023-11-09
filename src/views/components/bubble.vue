7e614244d3d37c5d9f450852ebfad730e3df1e5a
green1052 <green1052@pm.me>
2023-11-09 23:45:48 +0900

    차단 탭 디시콘 안보이는 오류 수정

--- src/views/components/bubble.vue
+++ src/views/components/bubble.vue
@@ -1,77 +1,86 @@
 <template>
-    <div class="refresher-bubble">
+  <div class="refresher-bubble">
         <span
             :class="{ image }"
             class="text"
             @click="safeTextClick">
-            <img
-                v-if="image"
-                :src="image" />
+          <img
+              v-if="image && image.startsWith('https://dcimg5.dcinside.com')"
+              :src="getDccon(image)"
+          />
+              <img
+                  v-else="image"
+                  :src="image"/>
+
             {{ text }} {{ extra ? ` (${extra})` : "" }}
             <span
                 v-if="gallery"
                 class="gallery"
-                >({{ gallery }})</span
+            >({{ gallery }})</span
             >
         </span>
-        <span
-            v-if="remove"
-            class="remove"
-            @click="safeRemoveClick">
+    <span
+        v-if="remove"
+        class="remove"
+        @click="safeRemoveClick">
             <svg
                 height="14"
                 viewBox="0 0 18 18"
                 width="14"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
-                    d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
+                    d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/>
             </svg>
         </span>
-    </div>
+  </div>
 </template>
 
 <script lang="ts">
-    import Vue from "vue";
+import Vue from "vue";
+import ky from "ky";
 
-    export default Vue.extend({
-        name: "refresher-bubble",
-        props: {
-            text: {
-                type: String
-            },
+export default Vue.extend({
+  name: "refresher-bubble",
+  props: {
+    text: {
+      type: String
+    },
 
-            image: {
-                type: String
-            },
+    image: {
+      type: String
+    },
 
-            isRegex: {
-                type: Boolean
-            },
+    isRegex: {
+      type: Boolean
+    },
 
-            gallery: {
-                type: String
-            },
+    gallery: {
+      type: String
+    },
 
-            extra: {
-                type: String
-            },
+    extra: {
+      type: String
+    },
 
-            remove: {
-                type: Function
-            },
+    remove: {
+      type: Function
+    },
 
-            textclick: {
-                type: Function
-            }
-        },
-        methods: {
-            safeTextClick() {
-                this.textclick?.();
-            },
+    textclick: {
+      type: Function
+    }
+  },
+  methods: {
+    async getDccon(url: string) {
+      return URL.createObjectURL(await ky.get(url).blob());
+    },
+    safeTextClick() {
+      this.textclick?.();
+    },
 
-            safeRemoveClick() {
-                this.remove?.();
-            }
-        }
-    });
+    safeRemoveClick() {
+      this.remove?.();
+    }
+  }
+});
 </script>
