57f9f1439f5d2b9b665c85f8cb373ef8989f157b
green1052 <green1052@pm.me>
2024-01-12 02:07:28 +0900

    webpack 설정 수정

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,12 @@
 const pkg = require("./package.json");
 const common = require("./webpack.common.js");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {merge} = require("webpack-merge");
 
 module.exports = (env) => {
     return merge(common, {
         mode: "development",
-        devtool: "cheap-module-source-map",
+        devtool: "inline-source-map",
         plugins: [
             new CopyWebpackPlugin({
                 patterns: [
@@ -30,11 +29,6 @@ module.exports = (env) => {
                         to: "assets/"
                     }
                 ]
-            }),
-            new HtmlWebpackPlugin({
-                template: "./src/views/index.html",
-                filename: "views/index.html",
-                inject: false
             })
         ]
     });
