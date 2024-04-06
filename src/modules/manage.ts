d69c43999c1c9a17d47737acc3a08d84edce10e7
green1052 <green1052@pm.me>
2024-04-07 01:11:09 +0900

    관리 모듈 추가

---
+++ src/modules/manage.ts
@@ -0,0 +1,13 @@
+export default {
+    name: "관리",
+    description: "무급 노예들을 위한 여러 편의 기능을 제공합니다.",
+    url: /\/board\/(view|lists)/,
+    enable: true,
+    default_enable: true,
+    func() {
+
+    },
+    revoke() {
+
+    }
+} as RefresherModule<{}>;
