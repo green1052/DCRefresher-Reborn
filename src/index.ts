fb4e2c0238e593c5acc0bd6feb9dc8f998495dbc
green1052 <green1052@pm.me>
2024-07-13 00:21:05 +0900

    logger 제거

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import "./styles/index.scss";
 import {filter} from "./core/filtering";
 import {modules} from "./core/modules";
-import log from "./utils/logger";
 import "./core/block";
 
 import "./core/updateCheck";
 
-log("🍊⚓ Initializing DCRefresher Reborn.");
+console.log("🍊⚓ Initializing DCRefresher Reborn.");
 
 const loadStart = performance.now();
 
@@ -15,7 +14,7 @@ const context = require.context("./modules/", true, /\.ts$/);
 Promise.all(context.keys().map((v) => modules.load(context(v).default))).then(
     () => {
         const took = (performance.now() - loadStart).toFixed(2);
-        log(`🍊✔️ DCRefresher Reborn Module Loaded. took ${took}ms.`);
+        console.log(`🍊✔️ DCRefresher Reborn Module Loaded. took ${took}ms.`);
 
         filter.run();
     }
--- src/utils/logger.ts
+++
@@ -1,8 +0,0 @@
-export default (...inputs: any[]): void => {
-    console.log(
-        `🔧 %c${new Date().toLocaleTimeString("en-US")} %c:`,
-        "color: #888;",
-        "color: unset;",
-        ...inputs
-    );
-};
