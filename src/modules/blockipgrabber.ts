e2e027a2799f369a54c75dcf6173e094206452a5
green1052 <green1052@pm.me>
2024-07-23 00:22:26 +0900

    1

--- src/modules/blockipgrabber.ts
+++ src/modules/blockipgrabber.ts
@@ -7,9 +7,9 @@ export default {
     enable: false,
     default_enable: false,
     settings: {
-        blockImageGrabber: {
-            name: "이미지 IP 추출 방지",
-            desc: "이미지를 불러올 때 다른 서버로의 요청을 차단하여 IP 추출을 방지합니다.",
+        blockMediaGrabber: {
+            name: "미디어 IP 추출 방지",
+            desc: "미디어를 불러올 때 다른 서버로의 요청을 차단하여 IP 추출을 방지합니다.",
             type: "check",
             default: false
         }
@@ -21,6 +21,6 @@ export default {
     }
 } as RefresherModule<{
     settings: {
-        blockImageGrabber: RefresherCheckSettings;
+        blockMediaGrabber: RefresherCheckSettings;
     }
 }>;
