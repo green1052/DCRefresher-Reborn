733782c3e068be2c4b52b8ecba15fe25a91ff68e
green1052 <green1052@pm.me>
2025-01-07 04:22:03 +0900

    1

--- src/assets/js/grecaptcha.ts
+++ src/assets/js/grecaptcha.ts
@@ -1,10 +1,9 @@
 $.getScript("https://www.google.com/recaptcha/api.js?render=6Lc-Fr0UAAAAAOdqLYqPy53MxlRMIXpNXFvBliwI", () => {
     window.addEventListener("message", (event) => {
         if (event.data.type === "refresherGrecaptcha" && event.data.action) {
-            grecaptcha.ready(() => {
-                grecaptcha.execute("6Lc-Fr0UAAAAAOdqLYqPy53MxlRMIXpNXFvBliwI", {action: event.data.action}).then((token) => {
-                    window.postMessage({type: "refresherGrecaptchaToken", token}, "*");
-                });
+            grecaptcha.ready(async () => {
+                const token = await grecaptcha.execute("6Lc-Fr0UAAAAAOdqLYqPy53MxlRMIXpNXFvBliwI", {action: event.data.action});
+                window.postMessage({type: "refresherGrecaptchaToken", token}, "*");
             });
         }
     });
