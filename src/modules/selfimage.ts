61e896e644984e122754cd6efd21565a970f03ed
green1052 <green1052@pm.me>
2025-01-27 18:10:27 +0900

    자짤 기능

---
+++ src/modules/selfimage.ts
@@ -0,0 +1,18 @@
+export default {
+    name: "자짤",
+    description: "자짤 기능을 추가합니다.",
+    url: /\/board\/(write|modify)/,
+    status: {},
+    memory: {},
+    enable: true,
+    default_enable: true,
+    settings: {},
+    require: ["filter"],
+    func(filter) {
+    },
+    revoke(filter) {
+    }
+} as RefresherModule<{
+    // settings: {};
+    require: ["filter"];
+}>;
