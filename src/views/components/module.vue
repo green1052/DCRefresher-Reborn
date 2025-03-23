f3c4e862750403c0c4417e3a06f0fca66f954201
green1052 <34261355++green1052@users.noreply.github.com>
2025-03-23 16:20:05 +0900

    다크 모드 정상화

--- src/views/components/module.vue
+++ src/views/components/module.vue
@@ -7,7 +7,9 @@
             <p class="desc">
                 {{ desc }}
             </p>
-            <p class="mute">요구 유틸 : {{ requirement?.join(", ") || "없음" }}</p>
+            <p class="mute">
+요구 유틸 : {{ requirement?.join(", ") || "없음" }}
+</p>
         </div>
         <div class="right">
             <refresher-checkbox
@@ -19,60 +21,54 @@
 </template>
 
 <script lang="ts">
-    import Vue from "vue";
-    import browser from "webextension-polyfill";
+import Vue from "vue";
+import browser from "webextension-polyfill";
 
-    import storage from "../../utils/storage";
-    import checkbox from "./checkbox.vue";
+import storage from "../../utils/storage";
+import checkbox from "./checkbox.vue";
 
-    export default Vue.extend({
-        name: "RefresherModule",
-        components: {
-            "refresher-checkbox": checkbox
+export default Vue.extend({
+    name: "RefresherModule",
+    components: {
+        "refresher-checkbox": checkbox
+    },
+    props: {
+        name: {
+            type: String,
+            required: true
         },
-        props: {
-            name: {
-                type: String,
-                required: true
-            },
 
-            desc: {
-                type: String,
-                required: true
-            },
-
-            requirement: {
-                type: Array,
-                required: false
-            },
-
-            enabled: {
-                type: Boolean,
-                required: true
-            }
+        desc: {
+            type: String,
+            required: true
         },
-        methods: {
-            update(_module, _key, value) {
-                storage.set(`${this.name}.enable`, value);
 
-                // TODO : 전체 로직 깔끔하게 변경
+        requirement: {
+            type: Array,
+            required: false
+        },
 
-                if (this.name === "다크 모드") {
-                    this.$root.$children[0].updateDarkMode(value);
-                }
-
-                browser.tabs.query({ active: true }).then((tabs) => {
-                    for (const tab of tabs) {
-                        browser.tabs.sendMessage(tab.id!, {
-                            type: "updateModuleStatus",
-                            data: {
-                                name: this.name,
-                                value: value
-                            }
-                        });
-                    }
-                });
-            }
+        enabled: {
+            type: Boolean,
+            required: true
         }
-    });
+    },
+    methods: {
+        update(_module, _key, value) {
+            storage.set(`${this.name}.enable`, value);
+
+            browser.tabs.query({ active: true }).then((tabs) => {
+                for (const tab of tabs) {
+                    browser.tabs.sendMessage(tab.id!, {
+                        type: "updateModuleStatus",
+                        data: {
+                            name: this.name,
+                            value: value
+                        }
+                    });
+                }
+            });
+        }
+    }
+});
 </script>
