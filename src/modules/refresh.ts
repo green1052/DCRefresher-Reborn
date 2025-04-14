2235e2a92443a268728ad0c4a398b3738942b223
green1052 <green1052@pm.me>
2025-04-15 01:41:35 +0900

    1

--- src/modules/refresh.ts
+++ src/modules/refresh.ts
@@ -266,7 +266,7 @@ export default {
                     if (!different.includes(no)) return;
 
                     $newListChildren
-                        .eq(index + newPostList.length - 1)
+                        .eq(index + newPostList.length)
                         .before($oldList.children().eq(index).addClass("refresher-deleted"));
                 });
             }
