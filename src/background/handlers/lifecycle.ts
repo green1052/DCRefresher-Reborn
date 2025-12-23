ad8903b108a7c3df264f4b4c802cbc93bc9e3270
green1052 <34261355+green1052@users.noreply.github.com>
2025-12-23 18:34:42 +0900

    1

--- src/background/handlers/lifecycle.ts
+++ src/background/handlers/lifecycle.ts
@@ -1,9 +1,7 @@
 import browser from "webextension-polyfill";
-
 import storage from "../../utils/storage";
 
 if (process.env.NODE_ENV === "production")
     browser.runtime.onInstalled.addListener(async (details) => {
-        const key = details.reason === "install" ? "refresher.firstInstall" : "refresher.updated";
-        await storage.set(key, true);
+        await storage.set(details.reason === "install" ? "refresher.firstInstall" : "refresher.updated", true);
     });
