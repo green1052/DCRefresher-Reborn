5c37f853af37c61927797a0408218d07b00276b8
green1052 <green1052@pm.me>
2026-08-20 05:12:11 +0900

    refactor(components): generate loader spinner blades with loops

--- src/components/loader.vue
+++ src/components/loader.vue
@@ -1,18 +1,11 @@
 <template>
     <transition name="refresher-opacity">
         <div class="refresher-loader spinner">
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
-            <div class="spinner-blade"/>
+            <div
+                v-for="i in 12"
+                :key="i"
+                class="spinner-blade"
+            />
         </div>
     </transition>
 </template>
@@ -36,64 +29,11 @@
     will-change: opacity;
 }
 
-.spinner .spinner-blade:nth-child(1) {
-    animation-delay: -1.6666666667s;
-    transform: rotate(30deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(2) {
-    animation-delay: -1.5833333333s;
-    transform: rotate(60deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(3) {
-    animation-delay: -1.5s;
-    transform: rotate(90deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(4) {
-    animation-delay: -1.4166666667s;
-    transform: rotate(120deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(5) {
-    animation-delay: -1.3333333333s;
-    transform: rotate(150deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(6) {
-    animation-delay: -1.25s;
-    transform: rotate(180deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(7) {
-    animation-delay: -1.1666666667s;
-    transform: rotate(210deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(8) {
-    animation-delay: -1.0833333333s;
-    transform: rotate(240deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(9) {
-    animation-delay: -1s;
-    transform: rotate(270deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(10) {
-    animation-delay: -0.9166666667s;
-    transform: rotate(300deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(11) {
-    animation-delay: -0.8333333333s;
-    transform: rotate(330deg) translate(0, -150%);
-}
-
-.spinner .spinner-blade:nth-child(12) {
-    animation-delay: -0.75s;
-    transform: rotate(360deg) translate(0, -150%);
+@for $i from 1 through 12 {
+    .spinner .spinner-blade:nth-child(#{$i}) {
+        animation-delay: #{($i - 21) * 0.0833333333}s;
+        transform: rotate(#{$i * 30}deg) translate(0, -150%);
+    }
 }
 
 .spinner.spinner-large {
