ad9670ca286fffdc6833eba78ef1137fbe2df8a6
green1052 <green1052@pm.me>
2026-09-25 04:15:28 +0900

    fix: emittery v2 event object in TypedEventBus wrapper

--- core/eventbus/bus.ts
+++ core/eventbus/bus.ts
@@ -6,7 +6,8 @@ export class TypedEventBus<M extends {[K in keyof M]: unknown[]}> {
     private readonly emitter = new Emittery<Record<string, unknown[]>>();
 
     public on<K extends keyof M & string>(event: K, callback: (...args: M[K]) => void): () => void {
-        return this.emitter.on(event, (args: unknown) => callback(...(args as M[K])));
+        // Emittery v2: 리스너가 {name, data} 객체를 받음
+        return this.emitter.on(event, ({data}) => callback(...(data as M[K])));
     }
 
     public emit<K extends keyof M & string>(event: K, ...args: M[K]): void {
